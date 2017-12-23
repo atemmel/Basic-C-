519cdbdcd49c7b62d4e0f4787d614d56fd73f025
atemmel <temmeladam@gmail.com>
2017-12-24 00:05:56 +0100

    Commented a few headers

--- addressbok/address_book.h
+++ addressbok/address_book.h
@@ -6,8 +6,29 @@
 #include <fstream>
 #include <vector>
 
+/**
+ *	För en användardialog och lägger därefter till en
+ *	kontakt till en fil.
+ *
+ * 	@param fileName Namnet på den fil kontakten skall läggas till i
+ */
 void addContact(const char * fileName);
+
+/**
+ *	För en användardialog och söker därefter efter
+ *	en kontakt i en fil.
+ *
+ *	@param fileName Namnet på den fil som skall sökas ur
+ */
 void searchContact(const char * fileName);
+
+/**
+ *	För en användardialog medans användaren får gå
+ *	igenom en fil men kontakter och välja vilka som
+ *	skall tas bort
+ *
+ *	@param fileName Namnet på den fil som kontakter skall tas bort ifrån
+ */
 void removeContact(const char * fileName);
 
 #endif
--- addressbok/filtered_input.h
+++ addressbok/filtered_input.h
@@ -4,10 +4,35 @@
 #include <iostream>
 #include <string>
 
+/*
+ *	Kontrollerar huruvida en sträng kan anses vara en 
+ *	godtycklig epostadress, tex:
+ *	abc@example.com
+ *
+ *	@param emailStr Strängen att undersöka
+ *	@return True om strängen kan vara en epostadress
+ */
 bool isValidEmail(const std::string & emailStr);
 
+/*
+ *	Parametrar till filteredInput:
+ *	ALPHA: Alfabetiska tecken, godkända namn
+ *	NUMERIC: Siffror, godkända telefonnummer
+ *	ALPHANUMERIC: Alphanumeriska tecken, Godkända meningar
+ *	MAIL: Godkända epostadresser
+ *	DATE: Godkända datum (YYYYMMDD)
+ *	BOOLEAN: y eller n (Ja eller nej))
+ */
 enum Filter {ALPHA, NUMERIC, ALPHANUMERIC, MAIL, DATE, BOOLEAN};
 
+/*
+ *	Tar användarinmatningar tills en sträng anses
+ *	tillfredsställande enligt ett filter
+ *
+ * 	@param input Den sträng som resultatet skall landa i
+ *	@param filter Vilket filter metoden skall arbeta efter
+ *	@param ignore De tecken metoden skall filtrera
+ */
 void filteredInput(std::string & input, const Filter & filter);
 void filteredInput(std::string & input, const std::string & ignore);
 
